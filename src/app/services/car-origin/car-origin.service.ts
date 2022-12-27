2f09a28b9595f18b0500561d1dc013b3e304600e
Esteban Manrupe <esteban_manrupe@hotmail.com>
2022-12-27 17:05:56 -0300

    finishing accordion

--- src/app/services/car-origin/car-origin.service.ts
+++ src/app/services/car-origin/car-origin.service.ts
@@ -7,8 +7,8 @@ import { CarOriginBrands } from 'src/app/interfaces/car-origin.interface';
 })
 export class CarOriginService {
   cars: CarOriginBrands = {
-    American: ['Chevrolet', 'Ford', 'GMC', 'Chrysler'],
-    Japanese: ['Toyota', 'Honda', 'Nissan', 'Suzuki'],
-    German: ['Volkswagen', 'Mercedes - Benz', 'Audi', 'BMW'],
+    American: ['Chevrolet', 'Ford', 'Chrysler'],
+    German: ['Volkswagen', 'Audi', 'BMW'],
+    Japanese: ['Toyota', 'Nissan'],
   };
 }
